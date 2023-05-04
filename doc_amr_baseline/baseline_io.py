68ca6a10025f50bea3bc9c641528abaad219b902
sadhana01 <sadhana_kumaravel@yahoo.co.in>
2023-05-04 18:23:34 -0400

    added node_id as var, recent member funcs
    Signed-off-by: sadhana01 <sadhana_kumaravel@yahoo.co.in>

--- doc_amr_baseline/baseline_io.py
+++ doc_amr_baseline/baseline_io.py
@@ -9,6 +9,40 @@ from collections import defaultdict
 alignment_regex = re.compile('(-?[0-9]+)-(-?[0-9]+)')
 class AMR2(AMR):
     @classmethod
+    def fix_alignments(self,removed_idx):
+        for node_id,align in self.alignments.items():
+            if align is not None:
+                num_decr = len([rm for rm in removed_idx if align[0]>rm])
+                if num_decr>0:
+                    lst = [x-num_decr for x in align]
+                    self.alignments[node_id] = lst 
+
+
+    @classmethod
+    def get_sen_ends(self):
+        self.sentence_ends = []
+        removed_idx = []
+        new_tokens = []
+        for idx,tok in enumerate(self.tokens):
+            if tok=='<next_sent>':
+                self.sentence_ends.append(len(new_tokens)-1)
+                removed_idx.append(idx)
+            else:
+                new_tokens.append(tok)
+        self.sentence_ends.append(len(new_tokens)-1)
+        self.tokens = new_tokens
+        self.fix_alignments(removed_idx)
+        
+    @classmethod
+    #FIXME
+    def remove_unicode(self):
+        for idx,tok in enumerate(self.tokens):
+            new_tok = tok.encode("ascii", "ignore")
+            self.tokens[idx] = new_tok.decode()
+            if self.tokens[idx]=='':
+                self.tokens[idx]='.'
+
+    @classmethod
     def get_all_vars(cls, penman_str):
         in_quotes = False
         all_vars = []
@@ -132,7 +166,8 @@ class AMR2(AMR):
         alignments = {}
         edges = []
         root = None
-
+        sentence = None
+    
         if 'short' in metadata:
             short_str = metadata["short"][0].split('\t')[1]
             short = eval(short_str)
@@ -142,6 +177,7 @@ class AMR2(AMR):
             short = None
             all_vars = AMR2.get_all_vars(penman_str)
         
+
         for key, value in metadata.items():
             if key == 'edge':
                 for items in value:
@@ -164,12 +200,11 @@ class AMR2(AMR):
                     if short is not None:
                         var = short[node_id]
                     else:
-                        var = AMR2.get_node_var(penman_str, node_id)
+                        var = node_id
                     if var is not None and var+" / " not in penman_str:
                         nvars[node_id] = None
                     else:
                         nvars[node_id] = var
-                    if var != None:
                         all_vars.remove(var)
             elif key == 'root':
                 root = value[0].split('\t')[1]
@@ -335,3 +370,112 @@ def read_amr_str_add_sen_id(amr_strs,doc_id,tokenize=False):
         raw_amrs[amr.sid] = amr
             
     return raw_amrs
+
+def recent_member_by_sent(chain,sid,doc_id):
+    def get_sid_fromstring(string):
+         
+        sid = [int(s) for s in re.findall(r'\d+', string)]
+        assert len(sid)==1
+        return sid[0]
+
+    sid = get_sid_fromstring(sid)    
+    diff = lambda chain : abs(get_sid_fromstring(chain[0].split('.')[0]) - sid)
+    ent = min(chain, key=diff)
+    fix = False
+    if get_sid_fromstring(ent[0].split('.')[0]) > sid:
+    #     print(doc_id," closest sent is higher than connecting node ",ent[0],sid)
+        fix = True
+    return ent[0]
+
+    
+
+def recent_member_by_align(chain,src_align,doc_id,rel=None):
+ 
+    diff = lambda chain : abs(chain[1]-src_align)
+    ent = min(chain, key=diff)
+    fix = False
+    if ent[1]>= src_align:
+    #     print(doc_id," coref edge missing ",ent[1],src_align,rel)
+        fix  = True      
+    return ent[0]
+
+#convert v0 coref edge to connect to most recent sibling in the chain
+def make_pairwise_edges(damr,verbose=False):
+    
+    ents_chain = defaultdict(list)
+    edges_to_delete = []
+    nodes_to_delete = []
+    doc_id = damr.amr_id
+    # damr.edges.sort(key = lambda x: x[0])
+    for idx,e in enumerate(damr.edges):
+        if e[1] == ':coref-of':
+            # if len(ents[e[2]])==0:
+                #damr.edges[idx] = (e[0],':same-as',ents[e[2]][-1])
+            # else:
+            edges_to_delete.append(e)
+
+            if e[0] in damr.alignments and damr.alignments[e[0]] is not None:
+                ents_chain[e[2]].append((e[0],damr.alignments[e[0]][0]))
+            else:
+                #FIXME adding the src node of a coref edge with no alignments member of chain with closest sid
+                # print(doc_id + '  ',e[0],' alignments is None  src node in coref edge, not adding it ')
+                sid = e[0].split('.')[0]
+                if len(ents_chain[e[2]]) >0 :
+                    ent = recent_member_by_sent(ents_chain[e[2]],sid,doc_id)
+                    damr.edges[idx] = (e[0],':same-as',ent)
+                #FIXME
+                else:
+                    
+                    print("coref edge missing, empty chain, edge not added")
+                
+            assert e[2].startswith('rel')
+       
+
+    
+    #adding coref edges between most recent sibling in chain    
+    for cents in ents_chain.values():
+        cents.sort(key=lambda x:x[1])
+        for idx in range(0,len(cents)-1):
+            damr.edges.append((cents[idx+1][0],':same-as',cents[idx][0]))
+
+    for e in edges_to_delete:
+        while e in damr.edges:
+            damr.edges.remove(e)
+
+    #connecting all other edges involving chain to most recent member in the chain
+    for idx,e in enumerate(damr.edges):
+        #Both src and target are coref nodes
+        if e[0] in ents_chain and e[2] in ents_chain:
+            damr.edges[idx] = (ents_chain[e[0]][-1][0],e[1],ents_chain[e[2]][-1][0])
+        
+        elif e[2] in ents_chain.keys():
+            #src node is a normal amr node
+            if e[0] in damr.alignments and damr.alignments[e[0]] is not None:
+                ent = recent_member_by_align(ents_chain[e[2]],damr.alignments[e[0]][0],doc_id,e[1])
+                
+            else:
+                #FIXME assigning src node with no alignments to the recent member by sent in the coref chain
+                # print(doc_id + '  ',e[0],' alignments is None ')
+                sid = e[0].split('.')[0]
+                ent = recent_member_by_sent(ents_chain[e[2]],sid,doc_id)
+            damr.edges[idx] = (e[0],e[1],ent)
+
+        elif e[0] in ents_chain.keys():
+            if e[2] in damr.alignments and damr.alignments[e[2]] is not None:
+                ent = recent_member_by_align(ents_chain[e[0]],damr.alignments[e[2]][0],doc_id,e[1])
+            else:
+                #FIXME assigning tgt node with no alignments to the recent member by sent in the coref chain
+                # print(doc_id + '  ',e[0],' alignments is None ')
+                sid = e[2].split('.')[0]
+                ent = recent_member_by_sent(ents_chain[e[0]],sid,doc_id)
+        
+            damr.edges[idx] = (ent,e[1],e[2])
+
+       
+    for n in ents_chain.keys():
+        while n in damr.nodes:
+            del damr.nodes[n]
+    
+        
+    
+    return damr
